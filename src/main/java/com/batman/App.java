2d1f5057e501acefe3c0e1f5eef47a2803e1824e
vishwas013 <gvishwassaigvishwassai@gmail.com>
2025-02-12 10:49:19 +0530

    first commit

---
+++ src/main/java/com/batman/App.java
@@ -0,0 +1,13 @@
+package com.batman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "I AM VENGENCE,I AM THE DARK KNIGHT" );
+    }
+}
